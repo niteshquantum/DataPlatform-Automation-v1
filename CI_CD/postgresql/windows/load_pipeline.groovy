94241047611968bc60c591384f2e351053bca342
niteshquantum <nitesh.patel@quantumatix.in>
2026-08-07 11:34:35 +0530

    modified this files

--- CI_CD/postgresql/windows/load_pipeline.groovy
+++ CI_CD/postgresql/windows/load_pipeline.groovy
@@ -225,30 +225,57 @@ pipeline {
         }
         stage('Schema Detection') {
 
+    steps {
+
+        script {
+
             runTrackedStage('Schema Detection') {
 
                 bat 'python scripts\\schema_detector.py postgresql'
 
             }
+
         }
 
-        stage('Datatype Detection') {
+    }
+
+}
+
+stage('Datatype Detection') {
+
+    steps {
+
+        script {
 
             runTrackedStage('Datatype Detection') {
 
                 bat 'python scripts\\datatype_registry_generator.py postgresql'
 
             }
+
         }
 
-        stage('Schema Editor') {
+    }
+
+}
+
+stage('Schema Editor') {
+
+    steps {
+
+        script {
 
             runTrackedStage('Schema Editor') {
 
                 bat 'python scripts\\schema_editor\\app.py postgresql'
 
             }
+
         }
+
+    }
+
+}
 
         stage('Create Database') {
 
