95e946d08e7177aad26b0afd879cabd6ec90131b
Tanishapanwar25 <tanisha.panwar@quantumatix.in>
2026-08-13 17:39:28 +0530

    Fix MySQL view template contract

--- scripts/python/common/objects/templates/mysql/view_template.py
+++ scripts/python/common/objects/templates/mysql/view_template.py
@@ -1,24 +1,6 @@
-LIQUIBASE_VIEW_TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
-
-<databaseChangeLog
-    xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="
-        http://www.liquibase.org/xml/ns/dbchangelog
-        http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-4.20.xsd">
-
-    <changeSet
-        id="{id}"
-        author="automation"
-        runOnChange="true">
-
-        <sqlFile
-            path="{sql_path}"
-            relativeToChangelogFile="false"
-            splitStatements="false"
-            stripComments="false"/>
-
-    </changeSet>
-
-</databaseChangeLog>
+VIEW_TEMPLATE = """CREATE OR REPLACE VIEW {view_name} AS
+SELECT
+    {columns}
+FROM {table_name}
+LIMIT {limit};
 """
